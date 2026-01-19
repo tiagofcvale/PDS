827563a5e3450c9711324573a95bb3c0d9a74053
tiagofcvale <tiagofcvale@ua.pt>
2026-01-19 18:48:35 +0000

    minecraft

--- Pratica/Melhoria/MinecraftPDS.java
+++ Pratica/Melhoria/MinecraftPDS.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.html.parser.Entity;
-
 public class MinecraftPDS {
     public static void main(String[] args) {
         WorldServer servidor = new WorldServer();
@@ -38,23 +36,177 @@ public class MinecraftPDS {
     }
 }
 
-interface Tool{
-    
+abstract class ToolDecorator implements Tool{
+    protected Tool decoratedTool;
+
+    public ToolDecorator(Tool decoratedTool) {
+        this.decoratedTool = decoratedTool;
+    }
+
+    @Override
+    public void use() {
+        decoratedTool.use();
+    }
 }
 
+class EfficiencyEnchantment extends ToolDecorator {
+
+    public EfficiencyEnchantment(Tool tool) {
+        super(tool);
+    }
+
+    @Override
+    public void use() {
+        super.use();
+        System.out.println("...com encatamento de eficiência!");
+    }
+
+}
+
+class SilkTouchEnchantment extends ToolDecorator {
+
+    public SilkTouchEnchantment(Tool tool) {
+        super(tool);
+    }
+
+    @Override
+    public void use() {
+        super.use();
+        System.out.println("...com encatamento de SilkTouch!");
+    }
+
+}
+
+interface Tool{
+    public void use();
+}
+
+class Pickaxe implements Tool{
+
+    @Override
+    public void use() {
+        System.out.println("Minerando blocos de pedra... ");
+    }
+
+}
+
+class Axe implements Tool{
+
+    @Override
+    public void use() {
+        System.out.println("Cortando madeira de carvalho... ");
+    }
+
+}
 
 interface Entity {
     public void setTool(Tool tool);
     public void interact();
+
+    public String getName();
+
+    public void sendGlobalMessage(String msg);
 }
 
 class Player implements Entity{
     private String name;
     private WorldServer server;
+    private Tool tool;
 
     public Player(String name, WorldServer server) {
         this.name = name;
         this.server = server;
+    }
+
+    @Override
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+
+    @Override
+    public void interact() {
+        tool.use();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorldServer getServer() {
+        return server;
+    }
+
+    public void setServer(WorldServer server) {
+        this.server = server;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    @Override
+    public void sendGlobalMessage(String msg) {
+        this.server.notifyAll(this,msg);
+    }
+
+    @Override
+    public String toString() {
+        return "Player [name=" + name + ", server=" + server + ", tool=" + tool + "]";
+    }
+}
+
+class Hostile implements Entity{
+    private String name;
+    private WorldServer server;
+    private Tool tool;
+
+    public Hostile(String name, WorldServer server) {
+        this.name = name;
+        this.server = server;
+    }
+
+    @Override
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+
+    @Override
+    public void interact() {
+        tool.use();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorldServer getServer() {
+        return server;
+    }
+
+    public void setServer(WorldServer server) {
+        this.server = server;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    @Override
+    public void sendGlobalMessage(String msg) {
+        this.server.notifyAll(this,msg);
+    }
+
+    @Override
+    public String toString() {
+        return "Hostile [name=" + name + ", server=" + server + ", tool=" + tool + "]";
     }
 }
 
@@ -83,7 +235,7 @@ class WorldServer {
     public void notifyAll(Entity e, String msg) {
         for (Entity entity : entidades) {
             if (!entity.equals(e)) {
-                System.out.println(e+msg);
+                System.out.println(e.getName()+": "+msg);
             }
         }
     }
